ad2fcd3655bae0f7b2be8f0ad9dfac9b161afb2d
Payden Dyer <paydendyer@yahoo.com>
2022-02-14 12:42:05 -0600

    Updates

---
+++ precache-manifest.cf47fb8c3ea24a159169d12ab7082c78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb9d589b5f9484dc59561d03b7aef0a",
+    "url": "/react-notes-app/index.html"
+  },
+  {
+    "revision": "772c0b1bf7f7788e8af7",
+    "url": "/react-notes-app/static/css/main.9727fa2a.chunk.css"
+  },
+  {
+    "revision": "5a172c427a50c616bf42",
+    "url": "/react-notes-app/static/js/2.c8be2fa0.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/react-notes-app/static/js/2.c8be2fa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "772c0b1bf7f7788e8af7",
+    "url": "/react-notes-app/static/js/main.6a4b7f2c.chunk.js"
+  },
+  {
+    "revision": "c3c86199df052fb44ae5",
+    "url": "/react-notes-app/static/js/runtime-main.0c76a5c2.js"
+  }
+]);
